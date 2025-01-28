f737b099a4a9f24ae2be24377715ff273529dbca
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2025-01-27 19:37:35 -0500

    Connected frontend and backend

--- PROG3270_GroupProject/Program.cs
+++ PROG3270_GroupProject/Program.cs
@@ -2,6 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Enable CORS (Cross-Origin Resource Sharing)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Allows only the React app to make requests
+              .AllowAnyMethod()  // Allows any HTTP method (GET, POST, etc.)
+              .AllowAnyHeader(); // Allows any header
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +28,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS policy
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
