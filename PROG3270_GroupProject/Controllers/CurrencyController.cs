77039393dde12f83d56a6f3f2f6c934ee37404a4
Selviniahh <xqwyss1699@gmail.com>
2025-02-17 13:45:00 -0500

    Currency implemented

---
+++ PROG3270_GroupProject/Controllers/CurrencyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PROG3270_GroupProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public CurrencyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCurrency()
+        {
+            var url = "https://api.frankfurter.app/latest?from=USD&to=CAD";
+            var response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Error fetching currency data");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var data = JsonSerializer.Deserialize<JsonElement>(content);
+            return Ok(data);
+        }
+    }
+}
