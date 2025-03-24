252949cf1c3c9d4e8224e8dc709c35d489ed4fdf
Selviniahh <xqwyss1699@gmail.com>
2025-03-24 18:48:42 -0400

    structure has been readjusted

---
+++ PROG3270_GroupProject/Infrastructure/Interfaces/IMemberService.cs
@@ -0,0 +1,12 @@
+﻿using PROG3270_GroupProject.Domain.Entities;
+using PROG3270_GroupProject.Models;
+
+namespace PROG3270_GroupProject.Application.Interfaces
+{
+    public interface IMemberService
+    {
+        Task<IEnumerable<Member>> GetAllMembersAsync();
+        Task AddMemberAsync(Member member);
+        Task<Member> GetMemberByCredentials(Login login);
+    }
+}
---
+++ PROG3270_GroupProject/Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using PROG3270_GroupProject.Domain.Entities;
+
+namespace PROG3270_GroupProject.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task AddProductAsync(Product product);
+    }
+}
